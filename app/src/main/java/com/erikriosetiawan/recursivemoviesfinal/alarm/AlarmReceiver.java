7018069bacf5b50d6c2c311e57867188ec878501
erik1997 <erikriosetiawan49@gmail.com>
2019-07-29 20:19:41 +0700

    Add Reminder

---
+++ app/src/main/java/com/erikriosetiawan/recursivemoviesfinal/alarm/AlarmReceiver.java
@@ -0,0 +1,186 @@
+package com.erikriosetiawan.recursivemoviesfinal.alarm;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.erikriosetiawan.recursivemoviesfinal.BuildConfig;
+import com.erikriosetiawan.recursivemoviesfinal.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import cz.msebera.android.httpclient.Header;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static final String API_KEY = BuildConfig.API_KEY;
+
+    public static final String TYPE_RELEASE = "ReleaseReminder";
+    public static final String TYPE_DAILY = "DailyReminder";
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String EXTRA_TYPE = "type";
+
+    public static final int ID_RELEASE = 100;
+    public static final int ID_DAILY = 101;
+
+    String TIME_FORMAT = "HH:mm";
+
+    public AlarmReceiver() {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String type = intent.getStringExtra(EXTRA_TYPE);
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+
+        String title = type.equalsIgnoreCase(TYPE_RELEASE) ? TYPE_RELEASE : TYPE_DAILY;
+        int notifId = type.equalsIgnoreCase(TYPE_RELEASE) ? ID_RELEASE : ID_DAILY;
+
+        if (notifId == ID_RELEASE) {
+            releaseMovie(context, notifId);
+        } else {
+            showAlarmNotification(context, title, message, notifId);
+        }
+    }
+
+    private void releaseMovie(final Context context, final int notifId) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&language=en-US";
+
+        Date date = Calendar.getInstance().getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        final String dateNow = dateFormat.format(date);
+
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject responseObject = new JSONObject(result);
+                    JSONArray list = responseObject.getJSONArray("results");
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject movie = list.getJSONObject(i);
+                        String releaseDate = movie.getString("release_date");
+
+                        if (releaseDate.equals(dateNow)) {
+                            showAlarmNotification(context, movie.getString("title"), movie.getString("title") + " is coming!", movie.getInt("id"));
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.d("Exception", e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("onFailure", error.getMessage());
+            }
+        });
+    }
+
+    private void showAlarmNotification(Context context, String title, String message, int notifId) {
+        String CHANNEL_ID = "Channel_1";
+        String CHANNEL_NAME = "Alarm Manager Channel";
+
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_timer_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmSound);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{1000, 1000, 1000, 1000, 1000});
+            builder.setChannelId(CHANNEL_ID);
+
+            if (notificationManagerCompat != null) {
+                notificationManagerCompat.createNotificationChannel(channel);
+            }
+        }
+        Notification notification = builder.build();
+        if (notificationManagerCompat != null) {
+            notificationManagerCompat.notify(notifId, notification);
+        }
+    }
+
+    public void setRepeatingAlarm(Context context, String type, String time, String message, int requestCode) {
+        if (isDateInvalid(time, TIME_FORMAT)) return;
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_TYPE, type);
+        String[] timeArray = time.split(":");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+        calendar.set(Calendar.SECOND, 0);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0);
+        if (alarmManager != null) {
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        }
+        Toast.makeText(context, type + " set up at " + time, Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelAlarm(Context context, String type) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        int requestCode = type.equalsIgnoreCase(TYPE_RELEASE) ? ID_RELEASE : ID_DAILY;
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0);
+        pendingIntent.cancel();
+        if (alarmManager != null) {
+            alarmManager.cancel(pendingIntent);
+        }
+        Toast.makeText(context, type + " Stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean isDateInvalid(String date, String format) {
+        try {
+            DateFormat dateFormat = new SimpleDateFormat(format, Locale.getDefault());
+            dateFormat.setLenient(false);
+            dateFormat.parse(date);
+            return false;
+        } catch (ParseException e) {
+            return true;
+        }
+    }
+
+    public boolean isWorking(Context context, String type) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        int requestCode = type.equalsIgnoreCase(TYPE_RELEASE) ? ID_RELEASE : ID_DAILY;
+        boolean isWork = (PendingIntent.getBroadcast(context, requestCode, intent, 0) != null);
+
+        return isWork;
+    }
+}
